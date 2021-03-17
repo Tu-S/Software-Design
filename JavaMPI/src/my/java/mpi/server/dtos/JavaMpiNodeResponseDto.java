75fc24b1b82bd76fa2f36a694a3fad4bcca2b1c0
Jlemix <unuashechka@gmail.com>
2021-03-17 09:56:44 +0700

    Delete garbage

--- JavaMPI/src/my/java/mpi/server/dtos/JavaMpiNodeResponseDto.java
+++
@@ -1,9 +0,0 @@
-package my.java.mpi.server.dtos;
-
-import java.io.Serializable;
-import java.util.UUID;
-
-public class JavaMpiNodeResponseDto implements Serializable {
-    public UUID id;
-    public byte[] answer;
-}
